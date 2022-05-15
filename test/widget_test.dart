019e9acd23ae05109a483ee435a0db7366e527a3
Brendaatto <71839478@certus.edu.pe>
2022-05-14 19:50:32 -0500

    cart
    carrito

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mediplants_app/main.dart';
+import 'package:mediplants_app/carrito.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
